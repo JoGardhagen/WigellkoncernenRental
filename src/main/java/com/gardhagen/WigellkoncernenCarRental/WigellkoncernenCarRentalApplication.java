52eeb87ddb2d887d6184e0c98184f98bbc01c13e
Joakim <superapa.spanx@gmail.com>
2022-10-01 11:13:05 +0200

    first commit

---
+++ src/main/java/com/gardhagen/WigellkoncernenCarRental/WigellkoncernenCarRentalApplication.java
@@ -0,0 +1,13 @@
+package com.gardhagen.WigellkoncernenCarRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellkoncernenCarRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellkoncernenCarRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gardhagen/WigellkoncernenCarRental/WigellkoncernenCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gardhagen.WigellkoncernenCarRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellkoncernenCarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
