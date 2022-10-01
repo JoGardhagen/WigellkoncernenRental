4273d4c3a6ebea09c0eb2baba3d1d595e00390fe
Joakim <superapa.spanx@gmail.com>
2022-10-01 19:07:54 +0200

    3

--- src/main/java/com/gardhagen/WigellkoncernenCarRental/models/cars/CarConfig.java
+++ src/main/java/com/gardhagen/WigellkoncernenCarRental/models/cars/CarConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
-
+//for testing
 @Configuration
 public class CarConfig {
 
