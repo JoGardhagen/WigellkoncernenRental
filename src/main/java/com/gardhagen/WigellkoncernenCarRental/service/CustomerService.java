896d1e8cab41cc57a3c8128c541c875124d9311a
Joakim <superapa.spanx@gmail.com>
2022-10-06 08:30:57 +0200

    1

--- src/main/java/com/gardhagen/WigellkoncernenCarRental/service/CustomerService.java
+++ src/main/java/com/gardhagen/WigellkoncernenCarRental/service/CustomerService.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class CustomerService implements CustomerServiceInterface{
     @Autowired
     private CustomerRepository customerRepository;
-
 //    @Autowired
 //    public CustomerService(CustomerRepository customerRepository){this.customerRepository = customerRepository;}
 
