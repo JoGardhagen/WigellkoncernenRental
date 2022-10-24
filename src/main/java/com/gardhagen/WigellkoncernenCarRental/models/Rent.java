bbce38d798db63ea9e998aa7e5ba9e324948ac3e
Joakim <superapa.spanx@gmail.com>
2022-10-24 19:42:06 +0200

    dateTimeAddedButWontsetOtherThenNull

--- src/main/java/com/gardhagen/WigellkoncernenCarRental/models/Rent.java
+++ src/main/java/com/gardhagen/WigellkoncernenCarRental/models/Rent.java
@@ -4,6 +4,8 @@ import com.gardhagen.WigellkoncernenCarRental.models.Car;
 import com.gardhagen.WigellkoncernenCarRental.models.Customer;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -29,19 +31,32 @@ public class Rent {
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    private String rentalDate;
+
 
     public Rent() {
     }
 
-    public Rent(long id, Car car, Customer customer) {
+    public Rent(Car car, Customer customer, String rentalDate) {
+        this.car = car;
+        this.customer = customer;
+        this.rentalDate = rentalDate;
+
+    }
+
+    public Rent(long id, Car car, Customer customer, String rentalDate) {
         this.id = id;
         this.car = car;
         this.customer = customer;
+        this.rentalDate = rentalDate;
     }
 
-    public Rent(Car car, Customer customer) {
-        this.car = car;
-        this.customer = customer;
+    public String getRentalDate() {
+        return LocalDateTime.now().toString();
+    }
+
+    public void setRentalDate(String rentalDate) {
+        this.rentalDate = LocalDate.now().toString();
     }
 
     public long getId() {
